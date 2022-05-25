fd8e72f293c4d89fca6fc1a82d4400b38bbd8ae5
Theo <tc17231@bristol.ac.uk>
2022-05-25 23:40:02 +0100

    small improvements

--- feebot.py
+++ feebot.py
@@ -5,7 +5,7 @@ from os import walk
 # ! set this to true to create the mark schemes
 create_markschemes = True 
 # ! set this to true to create the feedback
-create_feedback = False
+create_feedback = True
 
 # read correct feedback from file
 with open('feedback_correct.txt') as f:
@@ -36,7 +36,7 @@ files = []
 filenames = next(walk("./"), (None, None, []))[2] # This gives you all the files in the current directory
 for f in filenames:
     f = f.split('.')
-    if f[1] == 'pdf':
+    if len(f) == 2 and f[1] == 'pdf':
         files += [f[0]]
 #print(files)
 
@@ -45,7 +45,9 @@ for f in filenames:
 if create_markschemes:
     mark_scheme = Document('mark_scheme.docx')
     for f in files:
-        mark_scheme.save(f + '.docx')
+        if not f + '.docx' in filenames:
+            mark_scheme.save(f + '.docx')
+            #print('Created mark scheme for ' + f)
 
 if create_feedback:
     feedback = Document()
@@ -59,39 +61,43 @@ if create_feedback:
         total = 0
         score = 0
         num = 0 # keep track of the questions
-        for p in paragraphs:
-            text = p.text
-            if text[0] == 'Q': # for marks
-                #print(text)
-                # add question and mark in feedback
-                paragraph = feedback.add_paragraph(text+' - ')
-                # increase score
-                mark_text = text[4:].split('/')
-                score += int(mark_text[0])
-                total += int(mark_text[1])      
-            else: # for feedback
-                #print(p.text)
-                runs = p.runs
-                for r in runs:
-                    if r.font.highlight_color == 7: # add correct feedback and break
-                        #print(correct_feedback[num])
-                        paragraph.add_run(correct_feedback[num])
-                        num += 1  
-                        break
-                    elif r.font.highlight_color == 6: # add incorrect feedback and break
-                        #print(wrong_feedback[num])
-                        paragraph.add_run(wrong_feedback[num])
-                        num += 1  
-                        break  
-                    elif r.font.highlight_color == 4: # add feedback directly from mark sheet
-                        #print(r.text)
-                        paragraph.add_run(r.text)              
-                    elif r == runs[len(runs)-1]: # add missing feedback
-                        #print(missing_feedback[num])
-                        paragraph.add_run(missing_feedback[num])
-                        num += 1 
+        try:
+            for p in paragraphs:
+                text = p.text
+                if text[0] == 'Q': # for marks
+                    #print(text)
+                    # add question and mark in feedback
+                    paragraph = feedback.add_paragraph(text+' - ')
+                    # increase score
+                    split_text = text.split(':')
+                    mark_text = split_text[1].split('/')
+                    score += int(mark_text[0])
+                    total += int(mark_text[1])      
+                else: # for feedback
+                    #print(p.text)
+                    runs = p.runs
+                    for r in runs:
+                        if r.font.highlight_color == 7: # add correct feedback and break
+                            #print(correct_feedback[num])
+                            paragraph.add_run(correct_feedback[num])
+                            num += 1  
+                            break
+                        elif r.font.highlight_color == 6: # add incorrect feedback and break
+                            #print(wrong_feedback[num])
+                            paragraph.add_run(wrong_feedback[num])
+                            num += 1  
+                            break  
+                        elif r.font.highlight_color == 4: # add feedback directly from mark sheet
+                            #print(r.text)
+                            paragraph.add_run(r.text)              
+                        elif r == runs[len(runs)-1]: # add missing feedback
+                            #print(missing_feedback[num])
+                            paragraph.add_run(missing_feedback[num])
+                            num += 1 
 
-        # add total score in feedback
-        feedback.add_paragraph("Total: " + str(score) + '/' + str(total))
-        #feedback.save(f+'_feedback.docx')
+            # add total score in feedback
+            feedback.add_paragraph("Total: " + str(score) + '/' + str(total))
+            #feedback.save(f+'_feedback.docx')
+        except:
+            print('Error in file ' + f)
     feedback.save('full_feedback.docx')
